5e11a6d28b1dc6ddbb2398ad149da35848b1ad6e
John Spicer <john@edited.com>
2020-05-16 10:30:34 +0100

    Add comment about empty reducer console message

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,4 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 export default configureStore({
+	reducer: {
+		// This will throw a console error about no valid reducers,
+		// but I'm ok with it as I might want reducers in future.
+	}
 });
